f21873e02469b90ee34a4ef2b73e461067d3c3d0
ltigue <moneynloans@yahoo.com>
2014-07-18 06:23:28 -0400

    new MVC project
    created new project for class assignment

---
+++ homework/homework/Models/Part.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace homework.Models
+{
+    public class Part
+    {
+        public string PartID { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
